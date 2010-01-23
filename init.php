851c5ff09044dd029c96323edf1f9a60a42899a2
Michael Lavers <kolanos@gmail.com>
2010-01-23 13:26:40 -0800

    Initial commit

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Load Zend's Autoloader
+if ($path = Kohana::find_file('vendors', 'Zend/Loader'))
+{
+	ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(dirname($path)));
+	require_once 'Zend/Loader/Autoloader.php';
+	Zend_Loader_Autoloader::getInstance();
+}
