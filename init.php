a9cef371bc1682567ceb194236db7820484d5bef
Michael Lavers <kolanos@gmail.com>
2010-06-28 12:52:23 -0700

    Update ini.php

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load Zend's Autoloader
-if ($path = Kohana::find_file('vendors', 'Zend/Loader'))
+if ($path = Kohana::find_file('vendors', 'Zend/library/Zend/Loader'))
 {
 	ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(dirname($path)));
 	require_once 'Zend/Loader/Autoloader.php';
